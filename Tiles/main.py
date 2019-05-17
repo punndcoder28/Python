80490b88533bf88d1aaa54c58cc113560456e330
Puneeth K <puneethk.2899@gmail.com>
2019-05-17 22:16:18 +0530

    Finished the file to find the cost of tiling the entire room

---
+++ Tiles/main.py
@@ -0,0 +1,7 @@
+width = int(input("Enter the width of the room:"))
+
+height = int(input("Enter the height of the room:"))
+
+cost = int(input("Enter the cost of the tile per square:"))
+
+print("The cost to tile the entire room is ", (cost*(height*width)))
